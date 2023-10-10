6c412cf42cc58dd6cd495cba0dbc2b90958461cd
Markbaro <markbarasa2021@gmail.com>
2023-10-10 10:48:22 -0400

    first python console trial

---
+++ console.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import cmd
+"""
+Module  that contains the entry point of the python
+command interpreter
+"""
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    A simple command interpreter for the AirBnB project.
+    """
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """
+        Quit the command interpreter.
+        """
+        print("Goodbye!")
+        return True
+
+    def do_EOF(self, arg):
+        """
+        Quit the command interpreter on EOF.
+        """
+        print("exiting")
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
