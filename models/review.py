9b373efd93ad543bfc9c981a69ef8d4e7fb40357
Markbaro <markbarasa2021@gmail.com>
2023-10-10 04:48:41 -0400

    creating class review sublcass

--- models/review.py
+++ models/review.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """
-    Review class is a subclass of the BaseModel class with the public class attributes
+    Review class is a subclass of the BaseModel class
         place_id: string - empty string: it will be the Place.id
         user_id: string - empty string: it will be the User.id
         text: string - empty sting
