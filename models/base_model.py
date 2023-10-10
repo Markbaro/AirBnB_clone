bf1ca2daf141131ac8cdc48efde580682f67ef99
Markbaro <markbarasa2021@gmail.com>
2023-10-10 07:38:39 -0400

    add methods to filestorage

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,6 @@ class BaseModel:
     BaseModel class defines all available attributes
     and Methods for all subclasses and instances
     """
-    pass
 
     def __init__(self, *args, **kwargs):
         """
@@ -22,24 +21,48 @@ class BaseModel:
             2.created_at
             3.updated_at
         """
-        pass
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.created_at = datetime.strptime(value,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(value,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif key != "__class__":
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
         Overriding the toString method
         """
-        pass
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__,
+            self.id,
+            self.__dict__
+        )
 
     def save(self):
         """
         Sets the updated_at public instance attribute
         to the current date and time
         """
-        pass
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """
         Creates a dictionary from all the key-value pairs in
         the __dict__ attribute of the current BaseModel instance
         """
-        pass
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = str(__class__.__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        return (my_dict)
