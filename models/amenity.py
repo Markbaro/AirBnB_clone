38f39c3ff65b75de150cec9ec79ef4198b3307f0
Markbaro <markbarasa2021@gmail.com>
2023-10-10 04:54:23 -0400

    creating class amenity sublcass

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines class Amenity a subclass of BaseModel"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Class Amenity is a subclass of the BaseModel class
+    a) name : string - empty string
+    """
+
+    name = ''
